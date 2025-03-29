2372c314bf8882f0c8cf8747ffbc0ca64b019171
IlDavidone <144787644+IlDavidone@users.noreply.github.com>
2025-03-29 19:03:03 +0100

    Added js class selectors + small html changes

---
+++ script.js
@@ -0,0 +1,5 @@
+const colorSelector = document.querySelector("color-selector");
+const randomColor = document.querySelector("random-colors");
+const eraser = document.querySelector("eraser");
+const clearGrid = document.querySelector("clear");
+const gridSizeSelector = document.querySelector("grid-size-selector");
