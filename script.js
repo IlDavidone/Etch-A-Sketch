f0024f01d8bf56f921499fd199e6128280321fab
IlDavidone <144787644+IlDavidone@users.noreply.github.com>
2025-03-30 14:58:02 +0200

    Polished existing code + figured how to color grid elements (beta)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const DEFAULT_GRID_SIZE = 2;
+const DEFAULT_COLOR = '#f1f1f1';
+
 const colorSelector = document.querySelector(".color-selector");
 const randomColor = document.querySelector(".random-colors");
 const eraser = document.querySelector(".eraser");
@@ -10,14 +13,21 @@ const gridCell = document.querySelector(".grid-cell");
 
 let value;
 
-function createGridVertical(value) {
+function defaultGridSize(value) {
+    for(let i = 0; i < 2; i++){
+        createGridLayout(value);
+    }
+}
+
+function createGridLayout(value) {
   const gridElementVertical = document.createElement("div");
   gridElementVertical.classList.add("vertical-grid");
   grid.appendChild(gridElementVertical);
   function createGridElements(value) {
     for (u = 0; u < value; u++) {
       const gridCell = document.createElement("div");
-      gridCell.classList.add("grid-cell");
+      gridCell.classList.add("grid-cell", "grid-cell-border");
+      gridCell.addEventListener("mouseover", changeColor);
       gridElementVertical.appendChild(gridCell);
     }
   }
@@ -35,17 +45,33 @@ function removeGridChilds() {
     else return 0;
 }
 
+function changeColor(e) {
+    if(e.type == "mouseover"){
+        e.target.style.backgroundColor = "#000000";
+    };
+}
+
 gridSizeSelector.addEventListener("click", () => {
     removeGridChilds();
     value = prompt("Enter your preferred grid size (ex: 16)");
     for (i = 0; i < value; i++) {
-        createGridVertical(value);
+        createGridLayout(value);
       }
 });
 
 toggleGrid.addEventListener("click", () => {
     const gridCellItems = document.querySelectorAll(".grid-cell");
-        for(let i = 0; i < yeah.length; i++){
+        for(let i = 0; i < gridCellItems.length; i++){
             gridCellItems[i].classList.toggle("grid-cell-border");
         }
-})
+});
+
+colorSelector.addEventListener("click", () => {
+    colorValue = document.querySelector(".color-picker").value;
+});
+
+window.onload = () => {
+    defaultGridSize(DEFAULT_GRID_SIZE);
+}
+
+
